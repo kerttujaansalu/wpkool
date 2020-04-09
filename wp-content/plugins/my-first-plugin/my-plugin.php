a37ee0f250fc8ce67fcc9982da2cd2bc43cf2ee6
jaansalukerttuik <kerttu.jaansalu@khk.ee>
2020-04-09 15:54:29 +0300

    modified plugin `My First plugin` version 1.0 (after activation of `My First plugin` version 1.0)

---
+++ wp-content/plugins/my-first-plugin/my-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: My First plugin
+Plugin URI: http://jaansalukerttu.ikt.khk.ee/wordpress/
+Description: This is my first WordPress Plugin
+Author: Kerttu Jaansalu
+Author URI: http://jaansalukerttu.ikt.khk.ee/wordpress/
+Version: 1.0
+*/
