eb4278951f000ce0a81fc094dcb21551f4c911c8
jaansalukerttuik <kerttu.jaansalu@khk.ee>
2020-04-09 16:03:50 +0300

    modified plugin `My First plugin` version 1.0 (after activation of `My First plugin` version 1.0)

--- wp-content/plugins/my-first-plugin/very-first-plugin.php
+++ wp-content/plugins/my-first-plugin/very-first-plugin.php
@@ -7,3 +7,8 @@ Author: Kerttu Jaansalu
 Author URI: http://jaansalukerttu.ikt.khk.ee/wordpress/
 Version: 1.0
 */
+
+function dh_modify_read_more_link() {
+ return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
